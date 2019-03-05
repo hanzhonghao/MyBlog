3f65b2f3f66d5717c77545d4357e72a376107c90
hanzhonghao <hanzhonghao593513235@gmail.com>
2019-03-05 22:38:00 +0800

    add first page

---
+++ src/main/java/com/zhonghao/repository/UserRepository.java
@@ -0,0 +1,34 @@
+package com.zhonghao.repository;
+
+import com.zhonghao.domain.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    /**
+     * 创建或者修改用户
+     * @param user
+     * @return
+     */
+    User saveOrUpdateUser(User user);
+
+    /**
+     * 删除用户
+     * @param id
+     */
+    void deleteUser(Long id);
+
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
+    User getUserById(Long id);
+
+    /**
+     * 获取用户列表
+     * @return
+     */
+    List<User> listUsers();
+}
+
